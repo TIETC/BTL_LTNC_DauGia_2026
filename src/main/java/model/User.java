e7a8af190bcebfcd7b667ca33ae707f8018ad445
trungthanhtdn1612@gmail.com <trungthanhtdn1612@gmai.com>
2026-04-28 22:27:16 +0700

    Bổ sung class User

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,20 @@
 package model;
+public abstract class User extends Entity {
+    protected String username;
+    protected String password;
+    protected String email;
 
-public class User {
+    public User(String username, String password, String email) {
+        super();
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
 }
