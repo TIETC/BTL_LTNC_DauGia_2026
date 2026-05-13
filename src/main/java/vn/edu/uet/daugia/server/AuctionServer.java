4feed0f15097cb3ac8cbbdc3bd74abca11908fdd
trungthanhtdn1612@gmail.com <trungthanhtdn1612@gmai.com>
2026-05-14 01:10:59 +0700

    Bổ sung AuctionServer ( Trung )

---
+++ src/main/java/vn/edu/uet/daugia/server/AuctionServer.java
@@ -0,0 +1,40 @@
+package vn.edu.uet.daugia.server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class AuctionServer {
+
+    // Khai báo cổng mạng cho hệ thống
+    private static final int PORT = 5000;
+    // Bể chứa luồng (ThreadPool) - Giới hạn tối đa 100 người dùng cùng lúc
+    // Vượt quá 100 người, hệ thống sẽ cho vào hàng đợi (Queue) chứ không tạo thêm luồng
+    private static final ExecutorService pool = Executors.newFixedThreadPool(100);
+
+    public static void main(String[] args) {
+        System.out.println("=== HỆ THỐNG ĐẤU GIÁ SERVER ĐANG KHỞI ĐỘNG ===");
+        System.out.println("Đang mở cổng " + PORT + " và chờ Client kết nối...");
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+
+            // Luôn mở cửa đón khách
+            while (true) {
+                // Lệnh này là Blocking: Đứng đợi cho đến khi có khách gõ cửa
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("[KẾT NỐI MỚI] Khách hàng từ IP: " + clientSocket.getInetAddress().getHostAddress());
+
+                // Giao Client này cho ThreadPool xử lý
+                // ThreadPool sẽ gắp 1 luồng nhàn rỗi ra để chạy ClientHandler
+                pool.execute(new ClientHandler(clientSocket));
+            }
+
+        } catch (Exception e) {
+            System.err.println("Lỗi tại Server chính: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            pool.shutdown();
+        }
+    }
+}
