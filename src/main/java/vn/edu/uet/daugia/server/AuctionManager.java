174fd563beea069af636fe4baa4234cd8bef61f5
trungthanhtdn1612@gmail.com <trungthanhtdn1612@gmai.com>
2026-05-14 00:12:08 +0700

    Bổ sung ghi nhận và thông báo trong AuctionManager ( Trung )

--- src/main/java/vn/edu/uet/daugia/server/AuctionManager.java
+++ src/main/java/vn/edu/uet/daugia/server/AuctionManager.java
@@ -49,4 +49,20 @@ public class AuctionManager {
     public void removeAuction(String auctionId) {
         auctions.remove(auctionId);
     }
+
+    // 1. Cuốn sổ ghi danh sách những Client đang kết nối
+    // Dùng CopyOnWriteArrayList để chống lỗi Đa luồng khi có người thêm/xóa liên tục
+    private java.util.concurrent.CopyOnWriteArrayList<AuctionObserver> observers = new java.util.concurrent.CopyOnWriteArrayList<>();
+
+    // 2. Hàm đăng ký nhận thông báo
+    public void addObserver(AuctionObserver observer) {
+        observers.add(observer);
+    }
+
+    // 3. Hàm gọi khi có giá mới
+    public void notifyObservers(Auction auction) {
+        for (AuctionObserver obs : observers) {
+            obs.onNewBid(auction); // Báo cho từng người một
+        }
+    }
 }
