1c7adf03018bd369a0c15ad00ffac6ec6ae204cc
trungthanhtdn1612@gmail.com <trungthanhtdn1612@gmai.com>
2026-06-04 03:24:31 +0700

    Giao diện màn hình chính

--- src/main/java/vn/edu/uet/daugia/server/AuctionObserver.java
+++
@@ -1,8 +0,0 @@
-package vn.edu.uet.daugia.server;
-
-import vn.edu.uet.daugia.shared.model.Auction;
-
-public interface AuctionObserver {
-    // HHàm sẽ tự động gọi khi có một giá mới đặt thành công
-    void onNewBid(Auction auction);
-}
